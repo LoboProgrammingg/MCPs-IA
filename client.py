b171208f2d86ef30545d349dec4c45faacd720df
loboprogrammingg <matheusloboo2001@gmail.com>
2025-06-07 14:20:28 -0400

    Finalizando o client

--- client.py
+++ client.py
@@ -4,3 +4,14 @@ from pathlib import Path
 from fastmcp import Client
 
 caminho_servidor = Path(__file__).parent / 'servidor.py'
+cliente = Client(caminho_servidor)
+
+async def testar_servidor(cliente, nome_usuario, id_usuario):
+    async with cliente:
+        argumentos = {"nome_usuario": nome_usuario, "id_usuario": id_usuario}
+        resultado = await cliente.call_tool("dar_bom_dia", arguments=argumentos)
+        print(f'Resultado obtido do servidor MPC: {resultado}')
+        
+
+if __name__ == '__main__':
+    testar_servidor(cliente=cliente, nome_usuario='matheus', id_usuario=5)
