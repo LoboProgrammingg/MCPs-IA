8fea74764615ebc504da94b37b1cbb46600e82bd
loboprogrammingg <matheusloboo2001@gmail.com>
2025-06-07 14:09:13 -0400

    criando o primeiro decorador

--- servidor.py
+++ servidor.py
@@ -1,3 +1,7 @@
 from fastmcp import FastMCP
 
 servidor_mcp = FastMCP("mcp-teste")
+
+@servidor_mcp.tool()
+def dar_bom_dia(nome_usuario, id_usuario):
+    return f'Ola, {nome_usuario}! (ID {id_usuario})'
