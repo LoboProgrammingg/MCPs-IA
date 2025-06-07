6509ac5eb4164ccd6eafa00722242d76f674e598
loboprogrammingg <matheusloboo2001@gmail.com>
2025-06-07 14:11:54 -0400

    Criando tipagem, para contextualizar a IA, uma boa pratica para IA compreender melhor

--- servidor.py
+++ servidor.py
@@ -3,5 +3,9 @@ from fastmcp import FastMCP
 servidor_mcp = FastMCP("mcp-teste")
 
 @servidor_mcp.tool()
-def dar_bom_dia(nome_usuario, id_usuario):
+def dar_bom_dia(nome_usuario: str, id_usuario: int) -> str:
     return f'Ola, {nome_usuario}! (ID {id_usuario})'
+
+
+if __name__ == "__main__":
+    servidor_mcp.run(transport='stdio')
